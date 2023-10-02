e7641890863a3da74bc7405960a8fa52d815d5a6
Jose Ignacio Licona Orduña <jos.licona11@gmail.com>
2023-10-02 17:29:53 -0600

    rutas basicas con flutter

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -48,7 +49,12 @@ class _CustomListTitle extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: (){
-        // Todo: Navegar a otra pantalla
+        /* Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const ButtonsScreen(),
+          ),
+        ); */
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
