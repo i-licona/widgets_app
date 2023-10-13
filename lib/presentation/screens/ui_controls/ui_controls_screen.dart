b90cad1ee5906f8183875e20eed4953efebb2399
Nacho <i.licona@outlook.com>
2023-10-12 21:08:37 -0600

    definicion de pantallas de la app y barril actualizado

---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UIControlsAnimatedScreen extends StatelessWidget {
+  static const name = 'ui_controls_screen';
+  const UIControlsAnimatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
