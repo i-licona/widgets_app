d8d1b78ecafddfe926410552039bad5eb957b661
Nacho <i.licona@outlook.com>
2023-10-15 11:01:47 -0600

    progress indicator screen

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -20,18 +20,61 @@ class _ProgressView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    final colors = Theme.of(context).colorScheme;
+    return Center(
       child:  Column(
         children: [
-          SizedBox(height: 30,),
-          Text('Circular Progress Indicator'),
-          SizedBox(height: 10,),
+          const SizedBox(height: 30,),
+          const Text('Circular Progress Indicator'),
+          const SizedBox(height: 10,),
           CircularProgressIndicator(
             strokeWidth: 2,
+            color: colors.primary,
             backgroundColor: Colors.black45,
-          )
+          ),
+          const SizedBox(height: 20,),
+          const Text('Circular y linear indicator controlado'),
+          const SizedBox(height: 10,),
+          _ControllerProgressIndicator()
         ],
       ),
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value){
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot){
+        final progressValue = snapshot.data ?? 0;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 2,
+                color: colors.primary,
+                backgroundColor: Colors.black45,
+              ),
+              const SizedBox(width: 20,),
+              Expanded(
+                child: LinearProgressIndicator(
+                  value: progressValue,
+                ),
+              )
+            ],
+          ),
+        );
+      } 
     );
   }
 }
