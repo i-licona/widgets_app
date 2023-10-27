8a3d88ed240a58f26898b421c3f31c7b11de7426
Jose Ignacio Licona Orduña <jos.licona11@gmail.com>
2023-10-27 17:31:59 -0600

    animated container finalizado

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,8 +1,33 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.blue;
+  double borderRadius = 100.0;
+  
+  void changeShape(){
+    final random = Random();
+    width = random.nextInt(300) + 50;
+    height = random.nextInt(300) + 50;
+    borderRadius = random.nextInt(90) + 5;
+    color = Color.fromRGBO(
+      random.nextInt(255), 
+      random.nextInt(255), 
+      random.nextInt(255), 
+      1
+    );
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +39,17 @@ class AnimatedScreen extends StatelessWidget {
       body: Center(
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
-          curve: Curves.easeOutCubic,
-          width: 100,
-          height: 100,
+          curve: Curves.bounceOut,
+          width: width <= 0 ? 50 : width,
+          height: height <= 0 ? 50 : height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20)
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius <= 0 ? 50 : borderRadius) 
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () => changeShape(),
         backgroundColor: colors.primary,
         child: const Icon(
           Icons.play_arrow_rounded,
