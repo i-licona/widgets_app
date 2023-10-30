25c7af8c362e114247e7854a733d52bf016018ee
Jose Ignacio Licona Orduña <jos.licona11@gmail.com>
2023-10-29 19:26:51 -0600

    avance de infinite scroll

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,16 +1,71 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class InfiniteScrollScreen extends StatefulWidget {
   static const name = 'infinite_scroll_screen';
   const InfiniteScrollScreen({super.key});
-
+  
   @override
   State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
 }
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
-  List<int> imagesIds = [1,2,3,4,5,6];
+  List<int> imagesIds = [1,2,3,4,5];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
+  @override
+  void initState() {
+    super.initState(); 
+    scrollController.addListener(() {
+      if (((scrollController.position.pixels + 500)) >= scrollController.position.maxScrollExtent) {
+        loadNextPage();
+      }
+    }); 
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  Future loadNextPage() async{
+    if (isLoading == true) return;
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2));
+    addFiveImages();
+    isLoading = false;
+    // revisar si esta montado el componente / widget
+    if(!isMounted) return;
+    setState(() {});
+    //mover scroll
+  }
+
+  Future<void> onRefresh() async{
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 3));
+    if(!isMounted) return;    
+    isLoading = false;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+    setState(() {});
+  }
+
+  void addFiveImages(){
+    final lastId = imagesIds.last;
+    imagesIds.addAll(
+      [1,2,3,4,5].map((e) => lastId + e)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
@@ -23,26 +78,38 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          itemCount: imagesIds.length,
-          itemBuilder: (context, index) {
-            return FadeInImage(
-              width: double.infinity,
-              fit: BoxFit.cover,
-              height: 300,
-              placeholder:const AssetImage('assets/images/jar-loading.gif') ,
-              image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300') ,
-            );
-          },
+        child: RefreshIndicator(
+          edgeOffset: 10,
+          strokeWidth: 2 ,
+          onRefresh: () => onRefresh(),
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                width: double.infinity,
+                fit: BoxFit.cover,
+                height: 300,
+                placeholder:const AssetImage('assets/images/jar-loading.gif') ,
+                image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300') ,
+              );
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.pop(),
         backgroundColor: colors.primary,
-        child: const Icon(
-          Icons.arrow_back_ios_new_outlined,
-          color: Colors.white,
-        ),
+        child: isLoading 
+          ? const CircularProgressIndicator(
+              color: Colors.white,
+            )
+          : FadeIn(
+            child: const Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: Colors.white,
+              ),
+          ) ,
       ),
     );
   }
